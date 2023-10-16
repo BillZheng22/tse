db5807940361ac1fd7d0398af1f12f8e7b020e88
Bill Zheng <xml2963x@gmail.com>
2023-10-15 23:48:31 -0400

    Initial Commit: Finished Step 1

---
+++ crawler/crawler.c
@@ -0,0 +1,17 @@
+/* 
+ * crawler.c --- 
+ * 
+ * Author: William H. Zheng
+ * Created: 10-15-2023
+ * Version: 1.0
+ * 
+ * Description: 
+ * 
+ */
+
+#include <stdio.h>
+
+int main(){
+	printf("Hello World!\n");
+	return 0;
+}
---
+++ utils/README.md
@@ -0,0 +1,2 @@
+Group Members - Daniel Jeon, Walker Ball, Bill Zheng, Dhruv Chandra<br>
+Module 3 - queue and hash table modules
