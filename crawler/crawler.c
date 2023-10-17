6d80a7f1f88d5b023a6d481d3e2149193c3a7f89
Bill Zheng <xml2963x@gmail.com>
2023-10-17 16:14:48 -0400

    Debugging Step4 still

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -11,23 +11,55 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include <webpage.h>
 #include <queue.h>
 #include <hash.h>
 
+#define MAX_VISITED_URLS 1000
+
 void printURL(void *data){
+
+	fflush(stdout);
+	printf("Inside printURL function.\n");
+	fflush(stdout);
+	
 	webpage_t *page = (webpage_t *)data;
 	char *url = webpage_getURL(page);
-	webpage_delete(url);
+	//	webpage_delete((void *)url);
 	printf("Internal URL: %s\n", url);
 }
 
+bool searchURL(void *elementp, const void *key){
+
+	fflush(stdout);
+	printf("Inside searchURL function.\n");
+	fflush(stdout);
+
+	char *url = webpage_getURL((webpage_t *)elementp);
+	// webpage_delete((void *)url);
+	int cmp;
+
+	cmp = strcmp(url, (char *)key);
+
+	free(url);
+	
+	if (cmp != 0){
+		return false;
+	} 
+	return true;
+	
+	//	return (strcmp(url, (char *)key) == 0);
+}
+
 int main(){
-	int pos;
+	int pos = 0;
 	bool fetchResult;
-	char *seedURL, *html, *result;
+	char *seedURL, *result;
+	//	char *html;
 	webpage_t *page;
+	hashtable_t *visited;
 	queue_t* pageQueue;
 	
 	seedURL = "https://thayer.github.io/engs50/";
@@ -36,39 +68,46 @@ int main(){
 	// Fetch the webpage HTML
 	fetchResult = webpage_fetch(page);
 
-	pageQueue = qopen();
+	visited = hopen(100);
 	
+	pageQueue = qopen();
+	fflush(stdout);
+	printf("Start\n");
+	fflush(stdout);
 	if (fetchResult) {
 		// Fetch succeeded, print the HTML
-		html = webpage_getHTML(page);
+		//		html = webpage_getHTML(page);
 		//		printf("Webpage HTML:\n%s\n", html);
-		pos = 0;                
-	
 		while ((pos = webpage_getNextURL(page, pos, &result)) > 0) {
 
 			if (IsInternalURL(result)){
 				// Create a new webpage for the internal URL
-
-				webpage_t *internalPage = webpage_new(result, 0, NULL);
-				
-				qput(pageQueue, internalPage);
-				//printf("Internal ");
+				if (hsearch(visited, searchURL, result, strlen(result)) == NULL){
+					webpage_t *internalPage = webpage_new(result, 0, NULL);
+					qput(pageQueue, internalPage);
+					hput(visited, internalPage, result, strlen(result));
+				}
 			}
 			//else{
 				//				printf("External ");
 			//}
 			//printf("URL: %s\n", result);
+			fflush(stdout);
+			printf("While once\n");
+			fflush(stdout);
 			free(result);
 		}
-		//		free(result);
+		fflush(stdout);
+		printf("Visited Hash URLs:\n");
+		fflush(stdout);
+		happly(visited, printURL);
+		
+		hclose(visited);
+		webpage_delete(page);
+		exit(EXIT_SUCCESS);
 	} else {
-		free(result);
-		qclose(pageQueue);
-		return EXIT_FAILURE;
+		hclose(visited);
+		webpage_delete(page);
+		exit(EXIT_FAILURE);
 	}
-	qapply(pageQueue, printURL);
-	qclose(pageQueue);
-	webpage_delete(page);
-
-	return EXIT_SUCCESS;
 }
