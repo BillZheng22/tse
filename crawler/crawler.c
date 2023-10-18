767a122527b53c5d50bff9a8cad6e407814754c6
Bill Zheng <42471651+BillZheng22@users.noreply.github.com>
2023-10-18 17:13:39 -0400

    Bill's updates to crawler.c, some comments on the side.

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -22,37 +22,14 @@
 #define MAX_VISITED_URLS 1000
 
 void printURL(void *data){
-
-	fflush(stdout);
-	printf("Inside printURL function.\n");
-	fflush(stdout);
-	
 	webpage_t *page = (webpage_t *)data;
 	char *url = webpage_getURL(page);
-	//	webpage_delete((void *)url);
 	printf("Internal URL: %s\n", url);
 }
 
 bool searchURL(void *elementp, const void *key){
-
-	fflush(stdout);
-	printf("Inside searchURL function.\n");
-	fflush(stdout);
-
-	char *url = webpage_getURL((webpage_t *)elementp);
-	// webpage_delete((void *)url);
-	int cmp;
-
-	cmp = strcmp(url, (char *)key);
-
-	free(url);
-	
-	if (cmp != 0){
-		return false;
-	} 
-	return true;
-	
-	//	return (strcmp(url, (char *)key) == 0);
+	char *url = (char *)elementp;
+	return (strcmp(url, (char *)key) == 0);
 }
 
 int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
@@ -94,12 +71,11 @@ int main(){
 	int pos = 0;
 	bool fetchResult;
 	char *seedURL, *result;
-	//	char *html;
 	webpage_t *page;
 	webpage_t *temp; //for breadth depth
 	hashtable_t *visited;
 	queue_t* pageQueue;
-	webpage_t * internalPage; //internal page
+	webpage_t *internalPage; //internal page
 
 	seedURL = "https://thayer.github.io/engs50/";
 	page = webpage_new(seedURL, 0, NULL);
@@ -112,9 +88,6 @@ int main(){
 	
 	//queue of all pages
 	pageQueue = qopen();
-	fflush(stdout);
-	printf("Start\n");
-	fflush(stdout);
 
 	//breadth depth search method
 	if (fetchResult) {
@@ -133,29 +106,30 @@ int main(){
 					//put the new page in the queue
 					qput(pageQueue, internalPage);
 					//mark page as visited
-					hput(visited, internalPage, result, strlen(result));
+					hput(visited, internalPage, result, strlen(result)); //YO DJ, not sure if it should be internalPage or result again. 
+					//Check to see which one it should be!
 				}
 			}
-			//else{
-				//				printf("External ");
-			//}
-			//printf("URL: %s\n", result);
-			fflush(stdout);
-			printf("While once\n");
-			fflush(stdout);
-			free(result);
+			else{
+				free(result);
+			}
 		}
 		fflush(stdout);
 		printf("Visited Hash URLs:\n");
 		fflush(stdout);
 		happly(visited, printURL);
 		
-		hclose(visited);
+		while((internalPage = (qget(pageQueue))) != NULL){     //Bill added this in!                         
+      			webpage_delete(internalPage);                                                
+   		}       
 
+		qclose(pageQueue); //BILL added this in.
+		hclose(visited);
 		pagesave(page, 1, "pages");
 		webpage_delete(page);
 		exit(EXIT_SUCCESS);
 	} else {
+		qclose(pageQueue);
 		hclose(visited);
 		webpage_delete(page);
 		exit(EXIT_FAILURE);
