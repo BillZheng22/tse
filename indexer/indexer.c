269d115f2e517f6b350b68cbcee328c2a3091d6a
Bill Zheng <xml2963x@gmail.com>
2023-10-30 14:03:16 -0400

    wordmap is not storing the actual word.

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -26,31 +26,39 @@ typedef struct counter {
 } counter_t;
 
 typedef struct wordmap {
-  char * word; 
+  char * word;
   queue_t * doclist;
 } wordmap_t;
 
 static index_t* indexBuild(char* pageDirectory);
 static void indexPage(index_t* index, webpage_t* page, int id);
 
-bool wordSearch(void* elementp, const void* searchkeyp) {                     
-  //queue_t* queuep = (queue_t*) elementp;
-  if (elementp != NULL) {
+bool wordSearch(void* elementp, const void* searchkeyp) {
+  wordmap_t* wordmap;
+  printf("IN WORDSEARCH: %s\n", (char*)searchkeyp);
+
+  //HSEARCH PUTS THIS FUNCTION INTO ANOTHER QSEARCH 0x55c0f5814260
+  wordmap = (wordmap_t*) elementp;
+  printf("address of wordmap: %p\n", wordmap);
+  printf("wordmap key: %s\n", wordmap->word);
+  if (strcmp(wordmap->word,(char *)searchkeyp) == 0) {
+    printf("TRUE wordSearch.\n");
     return true;
   } else {
+    printf("FALSE wordSearch.\n");
     return false;
   }
 }
 
 bool queueSearch(void* elementp, const void* searchkeyp) {    
-  printf("In queueSearch.\n");                 
+  printf("In queueSearch.\n");
   counter_t* counter = (counter_t*) elementp;
   int searchint = *(int*)searchkeyp;
   if (counter->docid == searchint) {
-    printf("Finished queueSearch.\n");
+    printf("TRUE queueSearch.\n");
     return true;
   } else {
-    printf("Finished queueSearch.\n");
+    printf("FALSE queueSearch.\n");
     return false;
   }
 }
@@ -81,8 +89,6 @@ char* normalizeWord(char* word){
 
 int main(int argc, char * argv[]){
 
-    printf("Hello!\n");
-
     index_t* index = indexBuild("pages0");
 
     return 0;
@@ -110,50 +116,53 @@ void indexPage(index_t* index, webpage_t* page, int id){
     int pos = 0;
     char *word = NULL;
     int* idp = &id;
+    queue_t* queue;
+    counter_t* elem;
+    wordmap_t* wmap;
+    wordmap_t* wordmap;
+    counter_t* counter;
     
     while ((pos = webpage_getNextWord(page, pos, &word)) > 0) {
         if (normalizeWord(word) != NULL){
             printf("%s\n", word);
-            queue_t* queue;
-            counter_t* elem;
 
-            if (queue = (queue_t*)(hsearch(index, wordSearch, word, strlen(word))) != NULL) {
-              //hsearch returns a queue of wordMaps
-
-              //qsearch the queue of wordMaps for the word.
+            if ((wmap = (wordmap_t*)(hsearch((hashtable_t *)index, wordSearch, word, strlen(word)))) != NULL) {
+              //hsearch returns a wordMap that has the word as it's key.
+              fflush(stdout);
+              printf("FOUND in index.\n");
+              fflush(stdout);
+              ////qsearch the queue of wordMaps for the word.
                 //returns the wordMap structure
-                //take the wordMaps queue and do another qsearch for docID
-                  //will return counter structure
-                    //then ++ to that element.
-
-              if(queue){
-                printf("%p",&queue);
-              }
-              fflush(stdout);
-              printf("Finished hsearch.\n");
-              fflush(stdout);
-              if(elem = (counter_t*)(qsearch(queue, queueSearch, idp)) != NULL){
+              if((elem = (counter_t*)(qsearch(wmap->doclist, queueSearch, idp))) != NULL){
                 fflush(stdout);
                 printf("Finished qsearch.\n");
                 fflush(stdout);
                 elem->count += 1;
               }
+                //take the wordMaps queue and do another qsearch for docID
+                  //will return counter structure
+                    //then ++ to that element.
               printf("Succeeded hsearch and qsearch.\n");
             } else { //the word is not in the index yet
+              printf("NOT FOUND in index yet: %s\n", word);
               //open queue
-              //create counter with id and count
-              //add counter to the doclist
-              //create new wordMap
-              //add queue to a wordMap
-              //hput wordMap
-
-              printf("word is not in index yet.\n");
               queue = qopen();
-              counter_t* counter = (counter_t *) malloc(sizeof(counter_t));
+              //create counter with id and count
+              counter = (counter_t *) malloc(sizeof(counter_t));
               counter->docid = id;
               counter->count = 1;
+              //add counter to the doclist
               qput(queue, counter);
-              hput(index, queue, word, strlen(word));
+              //create new wordMap
+              wordmap = (wordmap_t *) malloc(sizeof(wordmap_t));
+              //add queue to the wordMap
+              wordmap->word = (char*) &word;
+              wordmap->doclist = queue;
+              printf("wordmap key: %s\n", wordmap->word);
+              //hput wordMap
+              hput(index, wordmap, word, strlen(word));
+              printf("address of wordmap: %p\n", wordmap);
+              //free(word);
             }
         }
         free(word);
