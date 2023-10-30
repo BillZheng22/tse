fb53a3f189cad14189a6640267bdc624deb1f070
Bill Zheng <xml2963x@gmail.com>
2023-10-29 23:46:22 -0400

    DJ and I figured it out, just need to code it.

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -18,27 +18,39 @@
 #include <hash.h>
 #include <pageio.h>
 #include <ctype.h>
+#include <index.h>
+
+typedef struct counter {
+  int docid;
+  int count;
+} counter_t;
 
 typedef struct wordmap {
   char * word; 
   queue_t * doclist;
 } wordmap_t;
 
-typedef struct counter {
-  char * docid; 
-  int count;
-} counter_t;
+static index_t* indexBuild(char* pageDirectory);
+static void indexPage(index_t* index, webpage_t* page, int id);
 
-bool matchingWordsSearch(void* elementp, const void* searchkeyp) {                     
-  wordmap_t *wordmap = (wordmap_t*) elementp;                                 
-  const char *key = (const char*) searchkeyp;                                          
-  if (elementp == NULL) {                                                               
-    return false;                                                                      
-  }
-  if (strcmp(wordmap->word, key) == 0) {
+bool wordSearch(void* elementp, const void* searchkeyp) {                     
+  //queue_t* queuep = (queue_t*) elementp;
+  if (elementp != NULL) {
     return true;
+  } else {
+    return false;
   }
-  else {
+}
+
+bool queueSearch(void* elementp, const void* searchkeyp) {    
+  printf("In queueSearch.\n");                 
+  counter_t* counter = (counter_t*) elementp;
+  int searchint = *(int*)searchkeyp;
+  if (counter->docid == searchint) {
+    printf("Finished queueSearch.\n");
+    return true;
+  } else {
+    printf("Finished queueSearch.\n");
     return false;
   }
 }
@@ -53,14 +65,14 @@ char* normalizeWord(char* word){
   }                                                                                    
                                                                                        
   // Check is word contains alphanumeric characters                                    
-  for ( cCounter = 0; cCounter < wordLen; cCounter++){                                 
+  for (cCounter = 0; cCounter < wordLen; cCounter++){                                 
     c = word[cCounter];                                                                
     if ( (isalpha(c)) == 0 ){                                                          
       return NULL;                                                                     
     }                                                                                  
   }                                                                                    
   // Check if word  is uppercase                                                       
-  for ( cCounter = 0; cCounter < wordLen; cCounter++){                                 
+  for (cCounter = 0; cCounter < wordLen; cCounter++){                                 
     c = tolower(word[cCounter]);                                                       
     word[cCounter] = c;                                                                
   }                                                                                    
@@ -71,35 +83,80 @@ int main(int argc, char * argv[]){
 
     printf("Hello!\n");
 
-    webpage_t *page = pageload(1, "pages0");
+    index_t* index = indexBuild("pages0");
 
-    hashtable_t *index = hopen(100); // change to argv[1] later
+    return 0;
+}
 
+static index_t* indexBuild(char* pageDirectory){
+    index_t* index = index_new(550); //between 300 and 900 slots 
+    int id = 1;
+
+    // char filename[50];
+    // sprintf(filename, "../%s/%d", pageDirectory, id);
+
+    // FILE * loadedFile;
+    // loadedFile = fopen(filename, "r");
+
+    webpage_t* page = pageload(id, pageDirectory);
+
+    indexPage(index, page, id);
+
+    webpage_delete(page);
+    return index;
+}
+
+void indexPage(index_t* index, webpage_t* page, int id){
     int pos = 0;
     char *word = NULL;
+    int* idp = &id;
     
     while ((pos = webpage_getNextWord(page, pos, &word)) > 0) {
         if (normalizeWord(word) != NULL){
             printf("%s\n", word);
+            queue_t* queue;
+            counter_t* elem;
 
-            wordmap_t * input = (wordmap_t *) malloc(sizeof(wordmap_t));
-            input->word = word;
+            if (queue = (queue_t*)(hsearch(index, wordSearch, word, strlen(word))) != NULL) {
+              //hsearch returns a queue of wordMaps
 
-            hput(index, input, input->word, strlen(input->word));
-            if (hsearch(index, matchingWordsSearch, input->word, strlen(input->word))!= NULL) {
-              input->doclist
+              //qsearch the queue of wordMaps for the word.
+                //returns the wordMap structure
+                //take the wordMaps queue and do another qsearch for docID
+                  //will return counter structure
+                    //then ++ to that element.
+
+              if(queue){
+                printf("%p",&queue);
+              }
+              fflush(stdout);
+              printf("Finished hsearch.\n");
+              fflush(stdout);
+              if(elem = (counter_t*)(qsearch(queue, queueSearch, idp)) != NULL){
+                fflush(stdout);
+                printf("Finished qsearch.\n");
+                fflush(stdout);
+                elem->count += 1;
+              }
+              printf("Succeeded hsearch and qsearch.\n");
+            } else { //the word is not in the index yet
+              //open queue
+              //create counter with id and count
+              //add counter to the doclist
+              //create new wordMap
+              //add queue to a wordMap
+              //hput wordMap
+
+              printf("word is not in index yet.\n");
+              queue = qopen();
+              counter_t* counter = (counter_t *) malloc(sizeof(counter_t));
+              counter->docid = id;
+              counter->count = 1;
+              qput(queue, counter);
+              hput(index, queue, word, strlen(word));
             }
-
-
-            free(word);
-        } else {
-            free(word);
         }
+        free(word);
         pos++;
     }
-
-    //webpage_delete(page);
-    free(page);
-
-    return 0;
 }
---
+++ utils/index.h
@@ -0,0 +1,18 @@
+#pragma once
+/*
+ * index.h
+ *
+ */
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct index index_t;
+index_t* index_new(const int numSlots);
+void index_delete(index_t* index);
+
+//bool index_set(index_t* index, char* word, int docID, int count);
+bool index_insert(index_t* index, int docID, char* word);
+
+// void index_save(index_t* index, char* file);
+// index_t* index_load(char* file);
+
